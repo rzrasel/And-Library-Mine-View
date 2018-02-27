7aceb80605dd64a9826308bb3365620d6f1beb2f
Developer <developer@Shurjo.local>
2018-02-27 16:16:46 +0600

    And Ware Adapter V-201802.00.4 - 2018-02-27 16:16:46

--- appusagesexample/src/main/java/com/rz/usagesexample/ActSplash.java
+++ appusagesexample/src/main/java/com/rz/usagesexample/ActSplash.java
@@ -1,13 +1,93 @@
 package com.rz.usagesexample;
 
+import android.app.Activity;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.rz.wareadapter.SparkedArrayAdapter;
+import com.rz.wareadapter.SparkedModelRowScope;
+import com.rz.wareadapter.SparkedModelRowViewFields;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class ActSplash extends AppCompatActivity {
+    private Activity activity;
+    private Context context;
+    private ListView sysLstView;
+    private SparkedArrayAdapter adapterListAdapter;
+    private ArrayList<SparkedModelRowViewFields> rowViewFieldListItems = new ArrayList<SparkedModelRowViewFields>();
+    private ArrayList<SparkedModelRowScope> modelDrawerMenuDataItems = new ArrayList<SparkedModelRowScope>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_splash);
+        activity = this;
+        context = this;
+        System.out.println("OPENED");
+        sysLstView = (ListView) findViewById(R.id.sysLstView);
+        //onSetRowViewField(SparkedArrayAdapter.FIELD_TYPE.TEXT_VIEW, "sysDrawerTitle");
+        //onSetRowViewField(SparkedArrayAdapter.FIELD_TYPE.TEXT_VIEW, "sysDrawerDescription");
+        //modelDrawerMenuDataItems.add(SparkedModelRowScope.onGetSetRow(argRowDataItems, (String) null, SparkedModelRowScope.LISTENER_TYPE.NONE));
+        onSetDrawerMenuItems();
+        adapterListAdapter = new SparkedArrayAdapter(context, R.layout.layout_list_row, modelDrawerMenuDataItems);
+        rowViewFieldListItems.add(adapterListAdapter.onSetRowViewField(SparkedArrayAdapter.FIELD_TYPE.TEXT_VIEW, "sysDrawerTitle"));
+        rowViewFieldListItems.add(adapterListAdapter.onSetRowViewField(SparkedArrayAdapter.FIELD_TYPE.TEXT_VIEW, "sysDrawerDescription"));
+        adapterListAdapter.onSetRowViewFieldList(rowViewFieldListItems);
+        sysLstView.setAdapter(adapterListAdapter);
+    }
+
+    public void onSetDrawerMenuItems() {
+        HashMap<String, String> eachRowDataItems = null;
+        eachRowDataItems = new HashMap();
+        eachRowDataItems.put("sysDrawerTitle", "Title-01");
+        eachRowDataItems.put("sysDrawerDescription", "Description-01");
+        //spinalRowDrawerDraw.spinalDrawerMenu.onSetItemData(eachRowDataItems, FirstFragment.class);
+        modelDrawerMenuDataItems.add(SparkedModelRowScope.onGetSetRow(eachRowDataItems, (String) null, SparkedModelRowScope.LISTENER_TYPE.NONE));
+        eachRowDataItems = new HashMap();
+        eachRowDataItems.put("sysDrawerTitle", "Title-02");
+        eachRowDataItems.put("sysDrawerDescription", "Description-02");
+        //spinalRowDrawerDraw.spinalDrawerMenu.onSetItemData(eachRowDataItems, SecondFragment.class);
+        modelDrawerMenuDataItems.add(SparkedModelRowScope.onGetSetRow(eachRowDataItems, (String) null, SparkedModelRowScope.LISTENER_TYPE.NONE));
+        //modelDrawerListItems = spinalRowDrawerDraw.spinalDrawerMenu.onGetDataList();
+    }
+
+    private void onSetAdapter() {
+        adapterListAdapter.onSetRowViewFieldList(rowViewFieldListItems)
+                .onSetRowViewFieldListenerHandler(new SparkedArrayAdapter.OnFieldListenerHandler() {
+                    @Override
+                    public void onSetFieldValue(ArrayList<SparkedModelRowViewFields> argRowViewFieldList, Object argObject) {
+                        SparkedModelRowScope itemScope = (SparkedModelRowScope) argObject;
+                        HashMap<String, String> hashMapRowIdValueItem = itemScope.getHashMapRowIdValueItems();
+                        for (SparkedModelRowViewFields itemField : argRowViewFieldList) {
+                            Object object = itemField.getFieldObject();
+                            String fieldResourceId = itemField.getFieldResourceId();
+                            if (object instanceof TextView) {
+                                TextView rowField = null;
+                                rowField = (TextView) itemField.getFieldObject();
+                                if (hashMapRowIdValueItem.containsKey(fieldResourceId)) {
+                                    rowField.setText(hashMapRowIdValueItem.get(fieldResourceId));
+                                }
+                                System.out.println(itemField.getFieldResourceId());
+                            }
+                            System.out.println("------" + fieldResourceId);
+                        }
+                    }
+                });
+    }
+
+    private void onSetRowViewField(SparkedArrayAdapter.FIELD_TYPE argFieldType, String argFieldResourceId) {
+        if (argFieldType == SparkedArrayAdapter.FIELD_TYPE.TEXT_VIEW) {
+            TextView textView = new TextView(context);
+            rowViewFieldListItems.add(SparkedModelRowViewFields.onGetSetModelRow(textView, argFieldResourceId));
+        } else if (argFieldType == SparkedArrayAdapter.FIELD_TYPE.IMAGE_VIEW) {
+            ImageView imageView = new ImageView(context);
+            rowViewFieldListItems.add(SparkedModelRowViewFields.onGetSetModelRow(imageView, argFieldResourceId));
+        }
     }
 }
--- librarywareadapter/src/main/java/com/rz/wareadapter/SparkedArrayAdapter.java
+++ librarywareadapter/src/main/java/com/rz/wareadapter/SparkedArrayAdapter.java
@@ -5,8 +5,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by Rz Rasel on 2018-02-27.
@@ -14,6 +17,7 @@ import java.util.ArrayList;
 
 public class SparkedArrayAdapter<T> extends ArrayAdapter<T> {
     private Context context;
+    //private static Context staticContext;
     private int layoutResourceId;
     public ArrayList<SparkedModelRowViewFields> rowViewFieldListItems = new ArrayList<SparkedModelRowViewFields>();
     private ArrayList<T> adapterListItems; // = new ArrayList<ModelInit>();
@@ -73,9 +77,47 @@ public class SparkedArrayAdapter<T> extends ArrayAdapter<T> {
             if (onFieldListenerHandler != null) {
                 onFieldListenerHandler.onSetFieldValue(rowViewFieldListItems, items);
             }
+            onSetRowViewFieldsValues(rowViewFieldListItems, items);
         }
         //System.out.println("|----|------------|AdapterData|----|" + argPosition);
         return rowViewRoot;
+    }
+
+    private void onSetRowViewFieldsValues(ArrayList<SparkedModelRowViewFields> argRowViewFieldList, Object argObject) {
+        SparkedModelRowScope itemScope = (SparkedModelRowScope) argObject;
+        HashMap<String, String> hashMapRowIdValueItem = itemScope.getHashMapRowIdValueItems();
+        for (SparkedModelRowViewFields itemField : argRowViewFieldList) {
+            Object object = itemField.getFieldObject();
+            String fieldResourceId = itemField.getFieldResourceId();
+            if (object instanceof TextView) {
+                TextView rowField = null;
+                rowField = (TextView) itemField.getFieldObject();
+                if (hashMapRowIdValueItem.containsKey(fieldResourceId)) {
+                    rowField.setText(hashMapRowIdValueItem.get(fieldResourceId));
+                }
+                //System.out.println(itemField.getFieldResourceId());
+            } else if (object instanceof ImageView) {
+                ImageView rowField = null;
+                rowField = (ImageView) itemField.getFieldObject();
+                if (hashMapRowIdValueItem.containsKey(fieldResourceId)) {
+                    int resourceId = Integer.valueOf(hashMapRowIdValueItem.get(fieldResourceId));
+                    rowField.setImageDrawable(context.getResources().getDrawable(resourceId));
+                }
+                System.out.println(itemField.getFieldResourceId());
+            }
+            //System.out.println("------" + fieldResourceId);
+        }
+    }
+
+    public SparkedModelRowViewFields onSetRowViewField(FIELD_TYPE argFieldType, String argFieldResourceId) {
+        if (argFieldType == FIELD_TYPE.TEXT_VIEW) {
+            TextView textView = new TextView(context);
+            return SparkedModelRowViewFields.onGetSetModelRow(textView, argFieldResourceId);
+        } else if (argFieldType == FIELD_TYPE.IMAGE_VIEW) {
+            ImageView imageView = new ImageView(context);
+            return SparkedModelRowViewFields.onGetSetModelRow(imageView, argFieldResourceId);
+        }
+        return null;
     }
 
     public void setSelectedPosition(int argPosition, boolean argIsClicked, int argCheckedColor) {
@@ -90,5 +132,19 @@ public class SparkedArrayAdapter<T> extends ArrayAdapter<T> {
         public void onSetFieldValue(ArrayList<SparkedModelRowViewFields> argRowViewFieldList, Object argObject);
         //public void onSetResourceLayout(int argLayoutResourceId);
         //Customer cust = (Customer) pObject;
+    }
+
+    public enum FIELD_TYPE {
+        TEXT_VIEW("text_view"),
+        IMAGE_VIEW("image_view");
+        private String fieldType;
+
+        FIELD_TYPE(String argFieldType) {
+            this.fieldType = argFieldType;
+        }
+
+        public String getFieldType() {
+            return this.fieldType;
+        }
     }
 }
